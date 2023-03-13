acb8d5c1a0ba2bd0d8197da8fd512485958b309b
Oscar Loaiza <oaloaiza9@misena.edu.co>
2023-03-13 11:07:29 -0500

    Creando folder actividad 10

---
+++ 10 - POO/02 - Ahorcado/Ahorcado.java
@@ -0,0 +1,14 @@
+public class Ahorcado{
+
+	private String palabra;
+	private char tablero [];
+
+	public Ahorcado(String palabra){
+		tablero = new char [palabra.length];
+	}
+
+	public void start(){
+		System.out.println(" --- metodo start ---");
+	}
+
+}
---
+++ 10 - POO/02 - Ahorcado/Principal.java
@@ -0,0 +1,10 @@
+public class Principal{
+	public static void main(String[] args) {
+		
+		System.out.println("==== INICIANDO EL JUEGO ===");
+		Ahorcado juego = new Ahorcado();
+		juego.start();
+		System.out.println("==== CERRANDO APLICACION ===");
+		
+	}
+}
