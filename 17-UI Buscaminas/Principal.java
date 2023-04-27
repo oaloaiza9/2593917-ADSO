168c01d5fdf1848782ec403e1e62926d05fedce4
oaloaiza9 <oaloaiza9@misena.edu.co>
2023-04-27 08:58:00 -0500

    Codigo Buscaminas para Terminar por Aprendices

---
+++ 17-UI Buscaminas/Principal.java
@@ -0,0 +1,9 @@
+
+public class Principal {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Buscaminas ventana_01 = new Buscaminas();
+    }
+    
+}
