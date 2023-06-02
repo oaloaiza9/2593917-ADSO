f09562182553ae791a183e4103e1357ff8d202fc
oaloaiza9 <oaloaiza9@misena.edu.co>
2023-06-02 09:01:13 -0500

    Agregando Vendedores a Facturador

--- 15-UI Factura/Principal.java
+++ 15-UI Factura/Principal.java
@@ -10,7 +10,12 @@ public class Principal{
 		listaClientes[3] = new Persona("108803", "Catalina Mendez", "Calle 17");
 		listaClientes[4] = new Persona("108804", "Andres Lopez", "Calle 16");
 
-
+		listaVendedores[0] = new Persona("109900", "Juan Castillo", "Calle 20");
+		listaVendedores[1] = new Persona("109901", "Ana Segura", "Calle 19");
+		listaVendedores[2] = new Persona("109902", "Julian Perez", "Calle 18");
+		listaVendedores[3] = new Persona("109903", "Carolina Tobon", "Calle 17");
+		listaVendedores[4] = new Persona("109904", "Carlos Perez", "Calle 16");
+		
 		Facturador inicio = new Facturador(listaClientes, listaVendedores);
 
 	}
